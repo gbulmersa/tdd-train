328478771b696d6734ee02ed34413e37ae66d3c1
gbulmersa <glen.bulmer@stateauto.com>
2017-03-22 11:23:51 -0400

    Updated to firefox

--- features/step_definitions/misc_steps.rb
+++ features/step_definitions/misc_steps.rb
@@ -5,16 +5,21 @@ require 'rubygems'
 Given(/^The user is on the page$/) do
   #pending # Write code here that turns the phrase above into concrete actions
 
-  browser = Selenium::WebDriver.for :chrome, switches: %w[--ignore-certificate-errors --disable-gpu --disable-popup-blocking --disable-translate]
+  #firefox
+  browser = Selenium::WebDriver.for :firefox
+
+  #chrome
+  #browser = Selenium::WebDriver.for :chrome, switches: %w[--ignore-certificate-errors --disable-gpu --disable-popup-blocking --disable-translate]
+  
   browser.navigate.to "http://tdd-train.com/tdd-train/rn.html"
   wait = Selenium::WebDriver::Wait.new(:timeout => 7)
   #puts "Test Passed: Found the word random friend in an anchor tag" if wait.until {
     #browser.find_element(:xpath => "//h1[contains(.,'random friend')]").displayed?
-    browser.find_element(:xpath => "//button").click
-    puts "Test Passed:" if wait.until {
-    (browser.find_element(:xpath => "//input[@type='text']").text != "")
+  browser.find_element(:xpath => "//button").click
+    #puts "Test Passed:" if wait.until {
+  (browser.find_element(:xpath => "//input[@type='text']").text != "")
     # browser.text_field(:id=> "result").value 
-    }
+    #}
   browser.quit
 
 end
