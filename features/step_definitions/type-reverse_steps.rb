7546d5f64e1e6086f83ba9a61fde792d84318d29
Weaver <WEA3765@corp.stateauto.com>
2017-03-22 15:27:17 -0400

    Jeffro

---
+++ features/step_definitions/type-reverse_steps.rb
@@ -0,0 +1,15 @@
+Given(/^the user is using our text reversal application$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the user provides the text that they want to be reversed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^the user asks for text reversal$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the text that the user provided is reversed$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
---
+++ features/type-reverse.feature
@@ -0,0 +1,9 @@
+Feature: Reverse the text that the user gives us
+
+
+Scenario: User provides text and asks it to be reversed
+
+	Given the user is using our text reversal application
+	When the user provides the text that they want to be reversed
+	And the user asks for text reversal
+	Then the text that the user provided is reversed
