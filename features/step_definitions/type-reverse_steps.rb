0c8192551ab16b2e27fd514e8ce1bfad82fde859
gbulmersa <glen.bulmer@stateauto.com>
2017-03-27 15:09:03 -0400

    Fixed reverse

--- features/step_definitions/type-reverse_steps.rb
+++ features/step_definitions/type-reverse_steps.rb
@@ -1,6 +1,7 @@
 require_relative '.\util.rb'
 
 browser = Util.load_page("http://23.23.241.33/tdd-train/tr.html")
+testReverse = "Jason"
 
 Given(/^the user is using our text reversal application$/) do
   true
@@ -9,7 +10,7 @@ end
 When(/^the user provides the text that they want to be reversed$/) do
   # Use find_element to get text input
  inputTextbox = browser.find_element(:xpath => "//input[@id='testtext']")
- inputTextbox.send_keys("silvian")
+ inputTextbox.send_keys(testReverse)
      
 end
 
@@ -25,8 +26,8 @@ Then(/^the text that the user provided is reversed$/) do
     element if element.displayed?
   }
   puts testResult.attribute("value")
-  puts (testResult.attribute("value") == "naivlis")
-  (testResult.attribute("value") == "naivlis")
+  puts (testResult.attribute("value") == testReverse.reverse!)
+  (testResult.attribute("value") == testReverse.reverse!)
 end
 
 #Util.clean_up(browser)
